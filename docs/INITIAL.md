65c8888311678b549cc075c2ca810c3c0cf18c7f
gabrielmelo04 <gmelo8720@gmail.com>
2026-03-20 21:28:39 -0300

    first commit

---
+++ docs/INITIAL.md
@@ -0,0 +1,8 @@
+# Criar o projeto:
+    - pnpm create vite@latest
+
+  ## Instalar o tailwindcss
+    - pnpm install tailwindcss @tailwindcss/vite
+
+  ## Instalar o Biome
+    - pnpm i -D @biomejs/biome
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-full flex h-screen bg-slate-900 justify-center items-center">
+      <h1 className="text-2xl text-slate-50 animate-bounce">Login</h1>
+    </div>
+  )
+}
