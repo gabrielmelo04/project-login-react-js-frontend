c0003e8dac4387f0701ff07792d7fa43105fd78a
gabrielmelo04 <gmelo8720@gmail.com>
2026-03-21 21:46:19 -0300

    chore: update src/routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,62 +1,64 @@
 import {
-    BrowserRouter,
-    Navigate,
-    Outlet,
-    Route,
-    Routes as RouterRoutes,
-} from "react-router-dom";
-import { PrivateLayout } from "../layouts/PrivateLayout";
-import { PublicLayout } from "../layouts/PublicLayout";
-import { LoginPage } from "../pages/login";
-import { NotFoundPage } from "../pages/not-found";
-import { DashboardPage } from "../pages/dashboard";
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes as RouterRoutes,
+} from 'react-router-dom'
+import { PrivateLayout } from '../layouts/PrivateLayout'
+import { PublicLayout } from '../layouts/PublicLayout'
+import { DashboardPage } from '../pages/dashboard'
+import { LoginPage } from '../pages/login'
+import { NotFoundPage } from '../pages/not-found'
+import { RegisterPage } from '../pages/register'
 
 function isAuthenticated() {
-    return false;
+  return false
 }
 
 function PrivateGuard() {
-    return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />;
+  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />
 }
 
 function PublicGuard() {
-    return isAuthenticated() ? <Navigate to="/" replace /> : <Outlet />;
+  return isAuthenticated() ? <Navigate to="/" replace /> : <Outlet />
 }
 
 function NotFoundByAuthLayout() {
-    if (isAuthenticated()) {
-        return (
-            <PrivateLayout>
-                <NotFoundPage />
-            </PrivateLayout>
-        );
-    }
-
+  if (isAuthenticated()) {
     return (
-        <PublicLayout>
-            <NotFoundPage />
-        </PublicLayout>
-    );
+      <PrivateLayout>
+        <NotFoundPage />
+      </PrivateLayout>
+    )
+  }
+
+  return (
+    <PublicLayout>
+      <NotFoundPage />
+    </PublicLayout>
+  )
 }
 
 export function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <RouterRoutes>
-                <Route element={<PublicGuard />}>
-                    <Route element={<PublicLayout />}>
-                        <Route path="/login" element={<LoginPage />} />
-                    </Route>
-                </Route>
+  return (
+    <BrowserRouter>
+      <RouterRoutes>
+        <Route element={<PublicGuard />}>
+          <Route element={<PublicLayout />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+        </Route>
 
-                <Route element={<PrivateGuard />}>
-                    <Route element={<PrivateLayout />}>
-                        <Route path="/" element={<DashboardPage />} />
-                    </Route>
-                </Route>
+        <Route element={<PrivateGuard />}>
+          <Route element={<PrivateLayout />}>
+            <Route path="/" element={<DashboardPage />} />
+          </Route>
+        </Route>
 
-                <Route path="*" element={<NotFoundByAuthLayout />} />
-            </RouterRoutes>
-        </BrowserRouter>
-    );
+        <Route path="*" element={<NotFoundByAuthLayout />} />
+      </RouterRoutes>
+    </BrowserRouter>
+  )
 }
