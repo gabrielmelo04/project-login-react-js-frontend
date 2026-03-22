12ef2f105b81ab44196b06598898beaa90c7edc2
gabrielmelo04 <gmelo8720@gmail.com>
2026-03-21 21:46:18 -0300

    chore: update src/layouts/PublicLayout.tsx

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -9,12 +9,9 @@ type PublicLayoutProps = {
 export function PublicLayout({ children }: PublicLayoutProps) {
   return (
     <div className="min-h-screen flex flex-col bg-slate-900 text-slate-50">
-        <main className="grow p-4">
+        <main className="grow">
             {children ?? <Outlet />}
         </main>
-        <footer className="bg-slate-900 text-slate-50 p-4 text-center"> 
-            &copy; 2026 My App. All rights reserved.
-        </footer>
     </div>
   );
 }
